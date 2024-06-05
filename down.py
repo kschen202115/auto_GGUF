6273e8ea0404c2a4778012097dc1fa1768baad4b
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2024-06-06 01:01:38 +0800

    Update down.py

--- down.py
+++ down.py
@@ -18,5 +18,5 @@ torch.set_default_device("cpu")
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
-model.save_pretrained("./saved_model")
-tokenizer.save_pretrained("./saved_model")
+model.save_pretrained("./md")
+tokenizer.save_pretrained("./md")
