d1d64ac55646898fd7c0eb69961f81d7f85b49ef
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2024-06-06 01:01:14 +0800

    Update down.py

--- down.py
+++ down.py
@@ -13,7 +13,7 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-torch.set_default_device("cuda")
+torch.set_default_device("cpu")
 
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
