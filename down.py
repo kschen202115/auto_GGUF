9b539ee30dad387d08ec63889f49269cc01ec303
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2024-06-06 00:19:36 +0800

    Update down.py

--- down.py
+++ down.py
@@ -1,26 +1,22 @@
-from huggingface_hub import snapshot_download
+# from huggingface_hub import snapshot_download
  
-from huggingface_hub import login
+# from huggingface_hub import login
  
 #目前需要输入access token确认登陆
-huggingface_hub.login("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")
+#huggingface_hub.login("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")
  
 #自行选择模型，自行修改下面参数（第一步的相对地址）
 
 #自行选择模型，自行修改下面参数（第一步的相对地址）
-model_addr = 'microsoft/phi-2'
+#model_addr = 'microsoft/phi-2'
  
-#提取模型库名和模型名称
-model_repo = model_addr.split('/')[0]
-model_name = model_addr.split('/')[1]
- 
-# 下载模型
-snapshot_download(
-repo_id=f"{model_addr}",
- 
-#去除tensorflow的模型，只下载pytorch模型
-ignore_patterns=["*.h5", "*.ot", "*.msgpack"],
- 
-#模型存储地址
-local_dir=f"md/",
-)
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+torch.set_default_device("cuda")
+
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+
+model.save_pretrained("./saved_model")
+tokenizer.save_pretrained("./saved_model")
